87bfe09e2fb38b547c1f146f79223c70d6b633b0
Ken Tucker <vb2ae@live.com>
2021-04-04 18:23:22 -0400

    initial commit

---
+++ WindowsFormsInConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsInConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MessageBox.Show("Test");
+        }
+    }
+}
